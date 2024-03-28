14f66b00d8411ba6cb62cea084b21cc1e9ce4a8b
geekhunger <hello@geekhunger.com>
2024-03-28 18:05:49 +0100

    tests

---
+++ example.js
@@ -0,0 +1,6 @@
+import * as fn from "./index.js"
+import rootpath from "app-root-path"
+
+// fn.createFolder(rootpath.resolve("/tmp/.foobar"), true) // inside sandbox
+// fn.createFolder("/tmp/.foobar", false, true) // outside sandbox
+// console.log(fn.absoluteSandboxPath("hey/foo/bar/baz/../../hello"))
